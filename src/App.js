27d6d2ba49c9ad3bea85d27dfe3acc8ba4dc696c
quan <quan93909@gmail.com>
2025-09-08 12:28:26 +0700

    Làm trang 404

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Error404 from "./pages/Error404";
 
 function App() {
   return (
@@ -11,9 +12,12 @@ function App() {
         <Route path="/" element={<Home />}/>
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
     </>
   );
 }
 
 export default App;
+
+//Ngoài những cái path được định nghĩa ra thì * là những cái path còn lại chưa được định nghĩa
