205553b788b55103d3fbbbc6c09b2064c5780fe8
quan <quan93909@gmail.com>
2025-09-08 14:29:30 +0700

    Route Objects

--- src/App.js
+++ src/App.js
@@ -1,44 +1,10 @@
 import './App.css';
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Error404 from "./pages/Error404";
-import LayoutDefault from './layout/LayoutDefault';
-import Blog from './pages/Blog';
-import BlogNews from './pages/Blog/BlogNews';
-import BlogRelated from './pages/Blog/BlogRelated';
-import BlogAll from './pages/Blog/BlogAll';
-import BlogDetail from './pages/Blog/BlogDetail';
-import Login from './pages/Login';
-import InfoUser from './pages/InfoUser';
-import PrivateRouters from './components/PrivateRouters';
+import AllRoute from './components/AllRoute';
 
 function App() {
   return (
     <>
-      <Routes>  
-        <Route path="/" element={<LayoutDefault />}>
-          <Route path="/" element={<Home />}/>
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-
-          <Route path="blog" element={<Blog />}>
-            <Route index element={<BlogAll />}/>
-            <Route path="news" element={<BlogNews />}/>
-            <Route path="related" element={<BlogRelated />}/>
-            <Route path=":id" element={<BlogDetail />}/>
-          </Route>  
-
-          <Route path="login" element={<Login />} />
-
-          <Route element={<PrivateRouters />} > 
-            <Route path="info-user" element={<InfoUser />} />
-          </Route>
-
-          <Route path="*" element={<Error404 />} />
-        </Route>
-      </Routes>
+      <AllRoute />
     </>
   );
 }
